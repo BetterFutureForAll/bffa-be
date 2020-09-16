221d9d8954b43a5ff31f802a048cb3110d9eb338
JOHN NELSON <JohnNelson4850@gmail.com>
2020-09-16 15:52:12 -0700

    dotenv

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
+require('dotenv').config();
 require('./lib/utils/connect')();
 
 const app = require('./lib/app');
-const PORT = 7891;
+const PORT = process.env.PORT || 7891;
 
 app.listen(PORT, ()=> {
 
